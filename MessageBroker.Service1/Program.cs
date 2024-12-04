4dd46df1d578a2e73feb8b16144b866a5b3e17ce
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-04 11:41:00 +0300

    MessageBroker sistemiyle direct exchange ile asenkron iletişim

---
+++ MessageBroker.Service1/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://dylxmqal:7_A9Vfo3nd5WpN62rVmsltBJBzUrG6SU@cow.rmq2.cloudamqp.com/dylxmqal");
+
+using IConnection connection =await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(exchange: "direct-exchange-example", routingKey: "direct-queue-example", body: byteMessage);
+}
+Console.Read();
---
+++ MessageBroker.Service2/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://dylxmqal:7_A9Vfo3nd5WpN62rVmsltBJBzUrG6SU@cow.rmq2.cloudamqp.com/dylxmqal");
+
+using IConnection connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+var name = await channel.QueueDeclareAsync();
+var queueName = name.QueueName;
+
+await channel.QueueBindAsync(
+    queueName,
+    exchange: "direct-exchange-example",
+    routingKey: "direct-queue-example");
+
+AsyncEventingBasicConsumer consumer = new(channel);
+
+await channel.BasicConsumeAsync(queueName, autoAck: true, consumer);
+
+consumer.ReceivedAsync += async (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
