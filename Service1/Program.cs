768fc7d80c2f69236d3ea443d00ee13f826434e1
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-04 09:46:52 +0300

    Add project files.

---
+++ Service1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using Service2;
+
+//var channel = GrpcChannel.ForAddress("http://localhost:5008");
+
+//var greetClient = new Greeter.GreeterClient(channel);
+
+//var response = greetClient.SayHello(new HelloRequest { Name = Console.ReadLine() });
+//await Task.Run(async () =>
+//{
+//    while (await response.ResponseStream.MoveNext(new CancellationToken())) ;
+//    Console.WriteLine($"Gelen Mesaj: {response.ResponseStream.Current.Message}");
+//});
+
+var channel = GrpcChannel.ForAddress("http://localhost:5008");
+var greetClient = new Greeter.GreeterClient(channel);
+
+var response = greetClient.SayHello(new HelloRequest { Name = Console.ReadLine() });
+
+while (await response.ResponseStream.MoveNext(CancellationToken.None))
+{
+    var message = response.ResponseStream.Current;
+    Console.WriteLine($"Gelen Mesaj: {message.Message}");
+}
+
+Console.Read();
